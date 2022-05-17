77c3b2960047dfba0ab07b5f232623102211a068
rakshanair <101237168+rakshanair@users.noreply.github.com>
2022-05-17 16:59:41 +0530

    Add files via upload

---
+++ restfulmemory/src/main/java/com/zensar/memory/user/Post.java
@@ -0,0 +1,47 @@
+package com.zensar.memory.user;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+@Entity
+public class Post 
+{
+@Id
+@GeneratedValue
+private Integer id;
+private String description;
+//many to one mapping 
+@ManyToOne(fetch=FetchType.LAZY)
+private User user;
+public Integer getId() 
+{
+return id;
+}
+public void setId(Integer id) 
+{
+this.id = id;
+}
+public String getDescription() 
+{
+return description;
+}
+public void setDescription(String description) 
+{
+description = description;
+}
+public User getUser() 
+{
+return user;
+}
+public void setUser(User user) 
+{
+this.user = user;
+}
+@Override
+public String toString() 
+{
+return String.format("Post [id=%s, description=%s]", id);
+}
+}
---
+++ restfulmemory/src/test/java/com/zensar/memory/RestfulmemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulmemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
